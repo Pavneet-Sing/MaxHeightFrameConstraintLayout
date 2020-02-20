93272cb3a323aa850e3feb4f2330bd364c588da1
Pavneet-Sing <dev.pavneet@gmail.com>
2020-02-20 21:46:44 +0530

    init repo

---
+++ app/src/main/java/com/pavneet_singh/demo/maxchildheightframelayout/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pavneet_singh.demo.maxchildheightframelayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
